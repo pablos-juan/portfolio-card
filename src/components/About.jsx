bbaa8f59debc413e840906768e916cdc249792d9
pablos-juan <pabloccsanchez@gmail.com>
2025-05-28 11:39:37 -0500

    Ajustar estilos en el responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,12 +2,12 @@ import { Undo2 } from 'lucide-react'
 
 export function About ({ setFlip }) {
   return (
-    <div className='md:grid md:place-items-center h-full w-full m-2'>
+    <div className='md:grid md:place-items-center h-full m-2'>
       <p
         className='flex items-center gap-1 bg-green-200 p-4 rounded-lg text-green-900 leading-4.5 font-medium cursor-pointer active:scale-105 hover:bg-white/80 transition-all duration-200'
         onClick={(prev) => setFlip(!prev)}
       >
-        <strong>Sobre mí,</strong>presione aquí para volver...
+        Sobre mí, presione aquí para volver...
         <Undo2 className='w-10 md:w-auto h-auto ml-2' />
       </p>
     </div>
