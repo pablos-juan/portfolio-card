8d6c1889a4434420cde439fd7ca9a2940ee80d4f
pablos-juan <pabloccsanchez@gmail.com>
2025-05-29 12:49:38 -0500

    Refactor componente About: ajustar estilos y eliminar código innecesario

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
-import { Hammer, Undo2 } from 'lucide-react'
+import { Undo2 } from 'lucide-react'
 
 export function About ({ setFlip }) {
   return (
-    <section className='h-full w-full p-6 flex flex-col gap-5'>
+    <section className='h-full w-full md:p-6 p-5 flex flex-col gap-4'>
       <div className='h-fit flex justify-between items-center'>
         <h2 className='text-6xl text-green-900 font-bold'>
           About
@@ -12,41 +12,24 @@ export function About ({ setFlip }) {
           className='flex items-center gap-1 bg-green-200 p-4 rounded-full text-green-900 leading-4.5 font-medium cursor-pointer active:scale-105 hover:bg-white/80 transition-all duration-200'
           onClick={(prev) => setFlip(!prev)}
         >
-          <Undo2 className='w-8 md:w-auto h-8' />
+          <Undo2 className='w-6 md:w-auto h-6' />
         </button>
       </div>
 
-      <div className='text-green-900 flex flex-wrap md:flex-nowrap md:gap-3 gap-10 h-full'>
-        <div className='flex flex-col gap-3 justify-between md:w-1/2 font-medium text-xl md:text-base'>
-          <div>
-            <p className='mb-2'>
-              Soy Juan Pablo, estudiante de <strong>Tecnología de Software en la Universidad Surcolombiana</strong>. Vivo en Neiva, aunque estoy <span className='text-teal-800 font-semibold'>dispuesto a cambiar de ciudad</span>. Empecé explorando lenguajes Backend, pero gracias a mis amigos descubrí mi pasión el <strong>desarrollo Frontend</strong>.
-            </p>
+      <div className='text-green-900 flex flex-col justify-between gap-6 md:gap-2 h-full'>
+        <p className='font-medium text-2xl leading-7.5 md:leading-6 md:text-lg'>
+          Soy Juan Pablo, estudiante de <strong>Tecnología de Software en la Universidad Surcolombiana</strong>. Vivo en Neiva, aunque estoy <span className='text-teal-700 font-semibold'>dispuesto a cambiar de ciudad</span>. Empecé explorando lenguajes Backend, pero gracias a mis amigos descubrí mi pasión el <strong>desarrollo Frontend</strong>.
+        </p>
 
-            <p>
-              Me esfuerzo por hacer bien mi trabajo, soy responsable, comprometido y <span className='text-teal-800 font-semibold'>sé adaptarme tanto al trabajo en equipo como a retos individuales</span>.
-            </p>
-          </div>
+        <div className='bg-white h-full' />
 
-          <div className='w-full h-12 bg-green-300/50 flex justify-between'>
-            <div className='h-full w-10 bg-linear-to-r from-green-400 to-transparent' />
-            <div className='w-full flex items-center justify-center'>
-              <p className='p-1 bg-green-900 text-white rounded'>
-                placeholder
-              </p>
-            </div>
-            <div className='h-full w-10 bg-linear-to-l from-green-400 to-transparent' />
-          </div>
-        </div>
-
-        <div className='md:w-1/2 bg-green-950/90 text-green-100 font-semibold rounded-lg p-4 flex flex-col justify-between'>
-          <h3 className='text-4xl flex items-center gap-3 font-bold'>
-            <Hammer className='h-7 w-7' />
+        <div className='bg-green-950/90 text-green-100 font-semibold rounded-lg p-3 flex flex-col md:flex-row justify-start gap-4'>
+          <h3 className='min-w-48 text-5xl md:text-4xl font-bold'>
             Que hago?
           </h3>
 
-          <div>
-            <p className='text-lg md:text-base mb-2'>
+          <div className='text-xl md:text-base'>
+            <p className='mb-2'>
               Me especializo en desarrollo Frontend y disfruto cuidar los detalles en cada proyecto. Uso tecnologías como <span className='text-white font-bold'>TypeScript, React y Astro</span>, además de Git y GitHub para organizar y documentar mi trabajo.
             </p>
 
